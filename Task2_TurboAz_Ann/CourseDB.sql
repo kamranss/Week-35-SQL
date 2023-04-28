88eae006da7301cf107c5e621c17112f57f6baf4
kamranss <kamranss@code.edu.az>
2023-04-29 01:49:12 +0400

    Refactor: Course db added

--- Task2_TurboAz_Ann/CourseDB.sql
+++ Task2_TurboAz_Ann/CourseDB.sql
@@ -1,24 +1,49 @@
-CREATE DATABASE Course
+
+CREATE DATABASE Coursee;
+
+USE Coursee
 
 CREATE TABLE Teachers(
  Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
  [Name] NVARCHAR(17),
  Surname NVARCHAR(17),
- Email NVARCHAR(35)  NOT NULL DEFAULT 0,
+ Email NVARCHAR(35) NOT NULL DEFAULT '',
  Age INT,
- Salary DECIMAL(10) 
+ Salary DECIMAL(10, 2)
 );
 
+INSERT INTO Teachers ([Name], Surname, Email, Age, Salary) VALUES 
+('ALI', 'ALIYEV', 'ali@gmail.com', 30, 700),
+('Arif', 'Arifli', 'arif@gmail.com', 18, 600),
+('Akif', 'Amalov', 'ali@gmail.com', 26, 1200),
+('Asim', 'Asimli', 'ali@gmail.com', 33, 1400),
+('Ahmat', 'Ahmatli', 'ali@gmail.com', 34, 1050),
+('Vahid', 'Vahidli', 'ali@gmail.com', 42, 3500),
+('Saleh', 'Salehli', 'ali@mail.ru', 50, 2900),
+('Yavuz', 'Yavuzlu', 'ali@gmail.com', 31, 5000),
+('Ikram', 'Bayramov', 'ali@mail.ru', 23, 2350),
+('Cavid', 'Bayramov', 'ali@gmail.com', 28, 4000),
+('Coshqun', 'Haciyev', 'ali@mail.ru', 20, 1670);
 
 
-INSERT INTO Teachers ('ALI', 'ALIYEV', 'ali@gmail.com', 15, '700');
-INSERT INTO Teachers ('Arif', 'Arifli', 'arif@gmail.com', 18, '600');
-INSERT INTO Teachers ('Akif', 'Amalov', 'ali@gmail.com', 15, '1200');
-INSERT INTO Teachers ('Asim', 'Asimli', 'ali@gmail.com', 15, '1400');
-INSERT INTO Teachers ('Ahmat', 'Ahmatli', 'ali@gmail.com', 15, '1050');
-INSERT INTO Teachers ('Vahid', 'Vahidli', 'ali@gmail.com', 15, '3500');
-INSERT INTO Teachers ('Saleh', 'Salehli', 'ali@gmail.com', 15, '2900');
-INSERT INTO Teachers ('Yavuz', 'Yavuzlu', 'ali@gmail.com', 15, '5000');
-INSERT INTO Teachers ('Ikram', 'Bayramov', 'ali@gmail.com', 15, '2350');
-INSERT INTO Teachers ('Cavid', 'Bayramov', 'ali@gmail.com', 15, '4000');
-INSERT INTO Teachers ('Coshqun', 'Haciyev', 'ali@gmail.com', 15, '1670');
+
+
+
+SELECT [Name], Surname, Age 
+FROM Teachers
+WHERE Age > (SELECT AVG(Age) FROM Teachers);
+
+
+SELECT [Name], Surname, Salary 
+FROM Teachers
+WHERE Salary BETWEEN 1000 AND 3000;
+
+
+SELECT [Name], Surname 
+FROM Teachers
+WHERE Email LIKE '%mail.ru';
+
+
+SELECT [Name], Surname 
+FROM Teachers
+WHERE [Name] LIKE 'C%' AND Surname LIKE 'C%';
