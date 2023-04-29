7985cef8966f8a8ecf79fd127e6d1c1d9f6ea640
kamranss <kamranss@code.edu.az>
2023-04-30 03:16:31 +0400

    Refactor: Task 3 added join feature

--- Task3_IMDB/Imdb_Db.sql
+++ Task3_IMDB/Imdb_Db.sql
@@ -1,144 +1,82 @@
-CREATE DATABASE TurboAzz
+CREATE DATABASE Imdb
 
-USE TurboAzz;
+USE Imdb;
 
-CREATE TABLE BanTypes(
+CREATE TABLE Directors(
  Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 [Name] NVARCHAR(30) NOT NULL);
 
-CREATE TABLE ColorTypes(
- Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-[Name] NVARCHAR(30) NOT NULL);
-
-CREATE TABLE UnitofMeasurement(
- Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-[Name] NVARCHAR(30) NOT NULL);
-
-CREATE TABLE [Ownership](
- Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-[Name] NVARCHAR(30) NOT NULL);
-
-CREATE TABLE FuelTypes(
- Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-[Name] NVARCHAR(30) NOT NULL);
-
-CREATE TABLE GearBoxTypes(
- Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-[Name] NVARCHAR(30) NOT NULL);
-
-CREATE TABLE WheelDriveSystem(
- Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-[Name] NVARCHAR(30) NOT NULL);
-
-CREATE TABLE CurrencyType(
- Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-[Name] NVARCHAR(30) NOT NULL);
-
-CREATE TABLE EngineSizeType(
- Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-[Name] NVARCHAR(30) NOT NULL);
-
-CREATE TABLE MarketType(
- Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-[Name] NVARCHAR(30) NOT NULL);
-
-CREATE TABLE SeatQuantities(
- Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-[Name] NVARCHAR(30) NOT NULL);
-
-CREATE TABLE Cities(
- Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-[Name] NVARCHAR(30) NOT NULL);
-
-
-CREATE TABLE Marks(
- Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-[Name] NVARCHAR(30) NOT NULL);
-
-CREATE TABLE Models(
- Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-[Name] NVARCHAR(30 ) NOT NULL,
-MarkId INT REFERENCES Marks(Id));
-
-CREATE TABLE Announces(
- Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
- Yurus INT NOT NULL,
- Price INT NOT NULL,
- CurrencyTypeId INT REFERENCES CurrencyType(Id),
- BanTypeId INT REFERENCES BanTypes(Id),
- ColorId INT REFERENCES ColorTypes(Id),
- YurusTypeId INT REFERENCES UnitofMeasurement(Id),
- OwnerShipId INT REFERENCES [OwnerShip](Id),
- FuelTypeId INT REFERENCES FuelTypes(Id),
- GearBoxTypeId INT REFERENCES GearBoxTypes(Id),
- OturucuTypeId INT REFERENCES WheelDriveSystem(Id),
- [Year] DECIMAL(4,0),
- EngineSizeId INT REFERENCES EngineSizeType(Id),
- MarketTypeId INT REFERENCES MarketType(Id),
- VinCode NVARCHAR(17),
- AdditionalInfo NVARCHAR(255),
- ModelTypeId INT REFERENCES Models(Id),
- HorsePower INT,
- SeatQuantityId INT REFERENCES SeatQuantities(Id)
-);
-
-CREATE TABLE Contacts(
+CREATE TABLE Films(
  Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 [Name] NVARCHAR(30) NOT NULL,
-Email NVARCHAR(30) NOT NULL,
-AnnId INT REFERENCES Announces(Id),
-CityId INT NOT NULL REFERENCES Cities(Id), 
-AnnounceId INT REFERENCES Announces(Id),
-);
+ImdbPoint Decimal(2,1)  NOT NULL,
+DurationValue TIME NOT NULL,
+DirectorId INT REFERENCES Directors(Id));
 
-
-CREATE TABLE VehicleCondition(
+CREATE TABLE Actors(
  Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
- IsDamaged  BIT DEFAULT 0,
- IsColorChanged  BIT DEFAULT 0,
- IsNeedRepair  BIT DEFAULT 0,
- IsCredit  BIT DEFAULT 0,
- IsBarter  BIT DEFAULT 0,
- AnnouncementId INT REFERENCES Announces(Id)
- );
+[Name] NVARCHAR(30) NOT NULL);
 
- CREATE TABLE FactoryFunctions(
+CREATE TABLE Genre(
  Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
- YungulLehimliDiskler  BIT DEFAULT 0,
- MerkeziQapanma  BIT DEFAULT 0,
- DeriSalon  BIT DEFAULT 0,
- OturacaqlarinVentilyasiyasi  BIT DEFAULT 0,
- [Abs]  BIT DEFAULT 0,
- ParkRadar BIT DEFAULT 0,
- KsenonLampalar BIT DEFAULT 0,
- Lyuk BIT DEFAULT 0,
- Kondisoner BIT DEFAULT 0,
- ArxaGoruntuKamerasi BIT DEFAULT 0,
- YagisSensoru BIT DEFAULT 0,
- OturacaqlarinIsidilmesi BIT DEFAULT 0,
- YanPerdeler BIT DEFAULT 0,
- AnnouncementId INT REFERENCES Announces(Id)
- );
+[Name] NVARCHAR(30) NOT NULL);
+
+CREATE TABLE ActorsPerFilm(
+ Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+ ActorsId INT REFERENCES Actors(Id),
+ FilmsId INT REFERENCES Films(Id));
+
+CREATE TABLE GenrePerFilm(
+ Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+ GenreId INT REFERENCES Genre(Id),
+ FilmsId INT REFERENCES Films(Id));
+
+SELECT Films.Name AS FilmName, Films.ImdbPoint, Genre.Name AS GenreName, Directors.Name AS DirectorName, 
+       STRING_AGG(Actors.Name, ', ') WITHIN GROUP (ORDER BY Actors.Name) AS ActorNames
+FROM Films
+JOIN GenrePerFilm ON Films.Id = GenrePerFilm.FilmsId
+JOIN Genre ON Genre.Id = GenrePerFilm.GenreId
+JOIN Directors ON Films.DirectorId = Directors.Id
+JOIN ActorsPerFilm ON Films.Id = ActorsPerFilm.FilmsId
+JOIN Actors ON Actors.Id = ActorsPerFilm.ActorsId
+WHERE Films.ImdbPoint > 6
+GROUP BY Films.Name, Films.ImdbPoint, Genre.Name, Directors.Name;
 
 
- SELECT * FROM Announces 
-JOIN BanTypes ON Announces.BanTypeId = BanTypes.Id 
-JOIN ColorTypes ON Announces.ColorId = ColorTypes.Id 
-JOIN UnitofMeasurement ON Announces.YurusTypeId = UnitofMeasurement.Id 
-JOIN [OwnerShip] ON Announces.OwnerShipId = [OwnerShip].Id 
-JOIN FuelTypes ON Announces.FuelTypeId = FuelTypes.Id 
-JOIN GearBoxTypes ON Announces.GearBoxTypeId = GearBoxTypes.Id 
-JOIN WheelDriveSystem ON Announces.OturucuTypeId = WheelDriveSystem.Id 
-JOIN CurrencyType ON Announces.CurrencyTypeId = CurrencyType.Id 
-JOIN EngineSizeType ON Announces.EngineSizeId = EngineSizeType.Id 
-JOIN MarketType ON Announces.MarketTypeId = MarketType.Id 
-JOIN SeatQuantities ON Announces.SeatQuantityId = SeatQuantities.Id 
-JOIN Models ON Announces.ModelTypeId = Models.Id 
-JOIN Contacts ON Announces.Id = Contacts.AnnounceId 
-JOIN Cities ON Contacts.CityId = Cities.Id 
-JOIN VehicleCondition ON Announces.Id = VehicleCondition.AnnouncementId 
-JOIN FactoryFunctions ON Announces.Id = FactoryFunctions.AnnouncementId 
-WHERE Announces.Id = [2];
+ SELECT Films.Name AS FilmName, Genre.Name AS GenreName, Films.ImdbPoint
+FROM Films
+JOIN GenrePerFilm ON Films.Id = GenrePerFilm.FilmsId
+JOIN Genre ON Genre.Id = GenrePerFilm.GenreId
+WHERE Genre.Name LIKE '%a%'
+
+
+ SELECT Films.Name AS FilmName, Films.DurationValue, Genre.Name AS GenreName, Films.ImdbPoint
+FROM Films
+JOIN GenrePerFilm ON Films.Id = GenrePerFilm.FilmsId
+JOIN Genre ON Genre.Id = GenrePerFilm.GenreId
+WHERE LEN(Films.Name) > 10 AND RIGHT(Films.Name,1) = 't';
+
+
+SELECT Films.Name AS FilmName, Genre.Name AS GenreName, Directors.Name AS DirectorName, Films.ImdbPoint, 
+       STRING_AGG(Actors.Name, ', ') WITHIN GROUP (ORDER BY Actors.Name) AS ActorNames
+FROM Films
+JOIN GenrePerFilm ON Films.Id = GenrePerFilm.FilmsId
+JOIN Genre ON Genre.Id = GenrePerFilm.GenreId
+JOIN Directors ON Films.DirectorId = Directors.Id
+JOIN ActorsPerFilm ON Films.Id = ActorsPerFilm.FilmsId
+JOIN Actors ON Actors.Id = ActorsPerFilm.ActorsId
+WHERE Films.ImdbPoint > (SELECT AVG(ImdbPoint) FROM Films)
+GROUP BY Films.Name, Genre.Name, Directors.Name, Films.ImdbPoint
+ORDER BY Films.ImdbPoint DESC;
+
+
+
+
+
+
+
+
+
 
 
 
