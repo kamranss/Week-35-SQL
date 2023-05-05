c95745bc5a7664ec49eaf158dfc9f90d31f8d681
kamranss <kamranss@code.edu.az>
2023-05-05 16:46:10 +0400

    Refactor: Designe updated

--- Task4_Spotify/Spotify.sql
+++ Task4_Spotify/Spotify.sql
@@ -25,3 +25,36 @@ CREATE TABLE Album_Music (
   FOREIGN KEY (AlbumId) REFERENCES Albums(Id),
   FOREIGN KEY (MusicId) REFERENCES Music(Id)
 );
+
+
+CREATE TABLE Artists (
+  Id INT NOT NULL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  dateOfBirth DATE NOT NULL,
+  musicGenre VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Artists_albums (
+  id INT NOT NULL PRIMARY KEY,
+  ArtistId INT NOT NULL,
+  AlbumId INT NOT NULL,
+  FOREIGN KEY (ArtistId) REFERENCES Artists(Id),
+  FOREIGN KEY (AlbumId) REFERENCES Albums(Id)
+);
+
+
+CREATE TABLE Users (
+  Id INT NOT NULL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  surname VARCHAR(255) NOT NULL,
+  dateOfBirth DATE NOT NULL,
+  email VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE LikedMusics (
+  Id INT NOT NULL PRIMARY KEY,
+  musicId INT NOT NULL,
+  UserId INT NOT NULL,
+  FOREIGN KEY (musicId) REFERENCES Music(Id),
+  FOREIGN KEY (UserId) REFERENCES Users(Id)
+);
